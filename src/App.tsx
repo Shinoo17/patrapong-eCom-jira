27388caca8a697c17f23240c77d921ec8424698f
Shinoo <patrapong17@gmail.com>
2025-05-23 10:39:18 +0700

    Test error lint

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogoX} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Add Items <span>remove Item</span></h1>
